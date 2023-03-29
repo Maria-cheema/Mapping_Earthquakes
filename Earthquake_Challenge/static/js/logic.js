96b03f6989500683fd0b624e74c07f38e34d2c86
maria <maria.sahi7@gmail.com>
2023-03-28 23:17:36 -0400

    Add Earthquake_Challenge folder and files

---
+++ Earthquake_Challenge/static/js/logic.js
@@ -0,0 +1,121 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+
+// Add GeoJSON data.
+let sanFranAirport =
+{"type":"FeatureCollection","features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"14",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375,37.61899948120117]}}
+]};
+
+
+
+
+// // Coordinates for each point to be used in the line.
+// let line = [
+//   [33.9416, -118.4085],
+//   [37.6214, -122.3790],
+//   [40.7899, -111.9791],
+//   [47.4502, -122.3088]
+// ];
+
+// // Create a polyline using the line coordinates and make the line red.
+// L.polyline(line, {
+//   color: "yellow"
+// }).addTo(map);
+
+// Get data from cities.js
+// let cityData = cities;
+
+// // Loop through the cities array and create one marker for each city.
+// cityData.forEach(city => {
+//     console.log(city);
+//     L.circleMarker(city.location, {
+//         radius: city.population/200000,
+//         color: "orange",
+//         fillColor: "orange",
+//         fillOpacity: 0.1,
+//         weight: 3
+//     })
+    
+//     .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+//   .addTo(map);
+// });
+
+
+
+// ------------------------ MODIFIED CODE ------------------------------------
+
+
+
+// We create the tile layer that will be the background of our map.
+// dark mode
+// let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}' , {
+    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// We create the dark view tile layer that will be an option for our map.
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Create a base layer that holds both maps.
+let baseMaps = {
+  Street: streets,
+  Dark: dark
+};
+
+// Create the map object with center, zoom level and default layer.
+let map = L.map('mapid', {
+  center: [44.0, -80.0],
+  zoom: 2,
+  layers: [dark]
+})
+
+// // Pass our map layers into our layers control and add the layers control to the map.
+ L.control.layers(baseMaps).addTo(map);
+
+
+// Then we add our 'graymap' tile layer to the map.
+// streets.addTo(map);
+
+// Accessing the airport GeoJSON URL
+// let airportData = "https://raw.githubusercontent.com/Maria-cheema/Mapping_Earthquakes/main/majorAirports.json";
+
+// Accessing the Toronto airline routes GeoJSON URL.
+let torontoData = "https://raw.githubusercontent.com/Maria-cheema/Mapping_Earthquakes/main/torontoRoutes.json";
+
+// Create a style for the lines.
+let myStyle = {
+  color: "#ffffa1",
+  weight: 2
+}
+
+// Grabbing our GeoJSON data.
+d3.json(torontoData).then(function(data) {
+  L.geoJson(data,{
+    style : myStyle,
+    onEachFeature : function(feature,layer) {
+  layer.bindPopup("<h3> Airline : "+ feature.properties.airline + "</h3> <hr> <h3> Destination:" +feature.properties.dst +"</h3>");
+  }
+  }).addTo(map);
+  });
